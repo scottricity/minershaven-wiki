31df8025b370f18d0575e656b1b345732eb7a138
Timothy3773 <jonathanscott122@gmail.com>
2023-01-01 17:06:33 -0500

    beta/ Startup

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __fileName = fileURLToPath(import.meta.url);
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import { Box } from "@mui/system";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Box>
+		<p>Hi</p>
+	  </Box>
+    </div>
+  );
+}
+
+export default App;
